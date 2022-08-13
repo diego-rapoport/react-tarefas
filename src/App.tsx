c1c68774ec5a1203d7b0b28dc0e9f03fb1bed174
Diego Rapoport <diegomrapoport@gmail.com>
2022-08-13 14:29:36 -0300

    v1 funcional do desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
+import './style.css'
+import { Tarefas } from "./components/Tarefas"
+import { TarefaProps } from './components/Tarefas'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tarefas, setTarefas] = useState<TarefaProps[]>([])
+
+  function criaTarefa() {
+    const inputTarefa = document.getElementById('tarefa') as HTMLInputElement
+    const nomeTarefa = inputTarefa.value
+    const novaTarefa = {
+      nome: nomeTarefa,
+    }
+
+    setTarefas([...tarefas, novaTarefa])
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <h1>Lista de Tarefas</h1>
+      <div className="addTarefa">
+        <input className="input" id="tarefa" placeholder="Nova tarefa" />
+        <button onClick={criaTarefa}>Adicionar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      {tarefas.map((tarefa, index) => {
+        return (
+          <Tarefas id={index} nome={tarefa.nome} />
+        )
+      })}
+
+    </>
   )
 }
 
